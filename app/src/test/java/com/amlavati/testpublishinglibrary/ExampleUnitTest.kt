a3ed7b38e5e20c28afa394092d7f850665364af4
sudarshan14 <sudarshan.bhatt12@gmail.com>
2024-02-07 12:40:28 +0530

    library show case

--- app/src/test/java/com/amlavati/testpublishinglibrary/ExampleUnitTest.kt
+++ app/src/test/java/com/amlavati/testpublishinglibrary/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.amlavati.testpublishinglibrary
 
+import com.amlavati.mathlibrary.Point
+import com.amlavati.mathlibrary.calculateDistanceTo
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,6 +14,10 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+        val point1 = Point(2.0, 3.0)
+        val point2 = Point(2.0, 3.0)
+        print(point1.calculateDistanceTo(point2))
+        assertEquals(4, 2 + 2)
         assertEquals(4, 2 + 2)
     }
 }
